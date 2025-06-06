2cd514905b7ff961388616a34ca09d1fa4c5bad9
Juanmi <jmcostalucena2000@gmail.com>
2025-06-06 18:26:48 +0200

    Eliminamos output y metodo openCart en Componente Navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CartItem } from '../../models/cart-item';
 
 @Component({
@@ -10,14 +10,5 @@ import { CartItem } from '../../models/cart-item';
 export class Navbar {
 
   @Input() items!: CartItem[];
-
-  @Output() openEventEmitter = new EventEmitter();
-
-  // Método para mostrar/ocultar el carrito
-  // Enviamos una señal al componente padre para que ejecute un evento (no enviamos valores)
-  // Así evitamos el error de que no conozca la propiedad openCart() en el componente hijo.
-  // El padre es el encargado de manejar el estado de la propiedad showCart.
-  openCart(): void {
-    this.openEventEmitter.emit()
-  }
+ 
 }
